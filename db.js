8d168e92f4825d677c02e30a68bf42984a73eed4
GabbyMcCarthy <gah1289@gmail.com>
2023-02-08 21:20:02 -0500

    added the rest of studies and routes to readme

--- db.js
+++ db.js
@@ -1,21 +1,22 @@
-"use strict";
-/** Database setup for jobly. */
-const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
+'use strict';
+/** Database setup for postharvest_app. */
+const { Client } = require('pg');
+const { getDatabaseUri } = require('./config');
 
 let db;
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
+if (process.env.NODE_ENV === 'production') {
+	db = new Client({
+		connectionString : getDatabaseUri(),
+		ssl              : {
+			rejectUnauthorized : false
+		}
+	});
+}
+else {
+	db = new Client({
+		connectionString : getDatabaseUri()
+	});
 }
 
 db.connect();
