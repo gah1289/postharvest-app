86d67130f555d3f422aaca9f1755336e18458541
GabbyMcCarthy <gah1289@gmail.com>
2023-02-14 11:14:13 -0500

    fixed studie sbugs

--- models/studies.js
+++ models/studies.js
@@ -147,7 +147,7 @@ class WindhamStudies {
 			return `Removed Windham Packaging study: ${id}`;
 		} catch (e) {
 			console.log({ e });
-			throw new NotFoundError(`Could not find id to delete: ${id}`);
+			throw new NotFoundError(e);
 		}
 	}
 }
